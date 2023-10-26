4f751866400512e1d36c6e47f273deffe8bee588
dcorry586 <dylancorr15@gmail.com>
2023-10-26 11:00:18 +0100

    tested frotend talking to backend by getting all delivery employees

---
+++ controller/deliveryEmployeeController.ts
@@ -0,0 +1,21 @@
+import { Application, Request, Response } from "express";
+import { DeliveryEmployee } from "../model/deliveryEmployee";
+import DeliveryEmployeeService from "../service/deliveryEmployeeService";
+ 
+module.exports = function(app: Application) {
+    
+    let employeeservice: DeliveryEmployeeService = new DeliveryEmployeeService();
+ 
+    app.get("/employees", async (req: Request, res: Response) => {
+        let employees: DeliveryEmployee[] = [];
+ 
+        try {
+           console.log("i am here!!") 
+            employees = await employeeservice.getAllDeliveryEmployees();
+        } catch (error) {
+            console.error(error);
+        }
+ 
+        res.render('test-delivery-employee', { employees } )
+    })
+}
