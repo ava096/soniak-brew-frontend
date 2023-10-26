6a7850ab6fae4a4a5a3b29d295bac7304fe82057
Jonny Evans <jonathan-evans99@hotmail.com>
2023-10-26 12:30:47 +0100

    View a singular delivery employee

--- controller/deliveryEmployeeController.ts
+++ controller/deliveryEmployeeController.ts
@@ -2,6 +2,8 @@ import { Application, Request, Response } from "express";
 import { DeliveryEmployee } from "../model/deliveryEmployee";
 import DeliveryEmployeeService from "../service/deliveryEmployeeService";
  
+const deliveryEmployeeService = require('../service/deliveryEmployeeService')
+
 module.exports = function(app: Application) {
     
     let employeeservice: DeliveryEmployeeService = new DeliveryEmployeeService();
@@ -18,4 +20,22 @@ module.exports = function(app: Application) {
  
         res.render('test-delivery-employee', { employees } )
     })
+
+    app.get('/deliveryEmployees/:id', async (req: Request, res: Response) =>{
+        let data = DeliveryEmployee;
+
+        try {
+            data = await deliveryEmployeeService.getDeliveryEmployeeById(req.params.id)
+
+            console.log(data)
+        } catch (e) {
+            console.error(e);
+        }
+
+        res.render('view-delivery-employee', { employees: data })
+    })
+
+
+
+
 }
--- model/deliveryEmployee.ts
+++ model/deliveryEmployee.ts
@@ -1,6 +1,7 @@
 export class DeliveryEmployee {
     delivery_employee_id?: number
     name?: string
+    salary?: number
     bank_account_number?: string 
     national_insurane_number?: string
 }
