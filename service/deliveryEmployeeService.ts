e01716b01a19e416b7c486cbc96ed2a1f089fe2d
dcorry586 <dylancorr15@gmail.com>
2023-10-26 15:45:05 +0100

    removed bracket from del employee service class

--- service/deliveryEmployeeService.ts
+++ service/deliveryEmployeeService.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { DeliveryEmployee } from "../model/deliveryEmployee";
 import { DeliveryEmployeeRequest } from "../model/deliveryEmployeeRequest";
-import validateDeliveryEmployee from "../validator/deliveryEmployeeValidator"; 
+import validateDeliveryEmployee from "../validator/deliveryEmployeeValidator";
 export default class DeliveryEmployeeService {
-    
-    
+
+
     async getAllDeliveryEmployees(): Promise<DeliveryEmployee[]> {
         try {
             const response = await axios.get("http://localhost:8080/api/deliveryEmployees");
             const employees: DeliveryEmployee[] = response.data;
-           console.log("employees -> ", employees); 
+            console.log("employees -> ", employees);
             return employees;
         } catch (e) {
             throw new Error("Unable to retrieve employees, " + e)
@@ -17,38 +17,36 @@ export default class DeliveryEmployeeService {
     }
 
 
-async createDeliveryEmployee(employee: DeliveryEmployeeRequest): Promise<number> {
-        const error: string = validateDeliveryEmployee(employee);     
+    async createDeliveryEmployee(employee: DeliveryEmployeeRequest): Promise<number> {
+        const error: string = validateDeliveryEmployee(employee);
         console.log("validation error: ", error);
         if (error) {
             throw new Error(error);
-        }        
+        }
 
-    try {
+        try {
             const response = await axios.post("http://localhost:8080/api/deliveryEmployees", employee);
             return response.data;
         } catch (e) {
             throw new Error("Could not create new employee")
         }
     }
-}
-
 
     async deleteDeliveryEmployee(employeeID: number): Promise<void> {
 
-        try {
-            await axios.delete(`http://localhost:8080/api/deliveryEmployees/${employeeID}`);
-        } catch (error) {
-            throw new Error("Failed to delete employee");
-        }
-
+    try {
+        await axios.delete(`http://localhost:8080/api/deliveryEmployees/${employeeID}`);
+    } catch(error) {
+        throw new Error("Failed to delete employee");
     }
+
+}
 }
 
 
-module.exports.getDeliveryEmployeeById = async function (id: number): Promise<DeliveryEmployee>{
+module.exports.getDeliveryEmployeeById = async function (id: number): Promise<DeliveryEmployee> {
     try {
-        const response = await axios.get('http://localhost:8080/api/deliveryEmployees/'+ id)
+        const response = await axios.get('http://localhost:8080/api/deliveryEmployees/' + id)
         console.log(response);
         return response.data
     } catch (e) {
